256e112fed62f92c66127d90fd56d313a1936977
Abdulla18787 <166361742+Abdulla18787@users.noreply.github.com>
2025-06-16 12:44:50 +0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,53 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.Timer")
+                .setHost(host,"xui_timer1")
+            );
+            
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("37.333333333333336em")
+                .setTop("24.38095238095238em")
+                .setSrc("{xui.ini.img_pic}")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setLeft("27.428571428571427em")
+                .setTop("23.61904761904762em")
+                .setCaption("Button")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button4")
+                .setLeft("16.761904761904763em")
+                .setTop("23.61904761904762em")
+                .setCaption("Status Button")
+                .setType("status")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input6")
+                .setLeft("16em")
+                .setTop("10.666666666666666em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Input")
+            );
+            
+            append(
+                xui.create("xui.UI.HiddenInput")
+                .setHost(host,"xui_ui_hiddeninput1")
+                .setValue("")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
